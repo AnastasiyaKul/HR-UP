7a4699115e10859bb3218e575e31d186386ad7bc
AnthonyGolo <abreydak@bk.ru>
2018-11-18 12:22:30 +0300

    Candidate-info components
    Added upload avatar component;
    added candidate description component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {
+  CreateItemTextComponent,
+  TextPopupComponent
+} from "./text-popup/create-item-text.component";
+import {MatDialogClose, MatDialogModule, MatFormFieldModule, MatInputModule} from "@angular/material";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateItemTextComponent,
+    TextPopupComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatInputModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [
+    TextPopupComponent,
+  ]
 })
 export class AppModule { }
