82b2b6e508ea8033f44834ba894a7621c3e64e22
AnastasiyaKul <43112477+AnastasiyaKul@users.noreply.github.com>
2018-12-23 22:59:57 +0300

    Привязала инфу о кандидатах к сервису и к карточкам, отредактировала отображение карточек

--- src/app/candidates-page/candidates-table/candidates-table.component.ts
+++ src/app/candidates-page/candidates-table/candidates-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CandidatesService} from '../../shared/candidates.service';
 
 @Component({
   selector: 'app-candidates-table',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CandidatesTableComponent implements OnInit {
 
-  constructor() { }
+  constructor(private candidatesService: CandidatesService) { }
 
   ngOnInit() {
   }
