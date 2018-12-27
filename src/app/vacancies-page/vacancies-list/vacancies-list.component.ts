cf23a867b985e35e225231aa8916ff503eb4a9dc
archibald2406 <archibald24062406@gmail.com>
2018-12-27 18:43:55 +0300

    Изменено добавление вакансий.

--- src/app/vacancies-page/vacancies-list/vacancies-list.component.ts
+++ src/app/vacancies-page/vacancies-list/vacancies-list.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {VacanciesService} from '../../shared/vacancies.service';
 import {VacancyListItem} from '../shared/templates';
+import {MatDialog} from '@angular/material';
+import {VacancyEditComponent} from '../../vacancy-page/vacancy-edit/vacancy-edit.component';
+import {Subject} from 'rxjs';
 
 @Component({
   selector: 'app-vacancies-list',
@@ -8,32 +11,49 @@ import {VacancyListItem} from '../shared/templates';
   styleUrls: ['./vacancies-list.component.css']
 })
 export class VacanciesListComponent {
-  vacancies: VacancyListItem[];
-  showOpened: boolean;
-  showSuspended: boolean;
-  showCanceled: boolean;
-  showClosed: boolean;
+  vacancies: VacancyListItem[] = [];
+  showOpened: boolean = true;
+  showSuspended: boolean = true;
+  showCanceled: boolean = true;
+  showClosed: boolean = true;
+  refresh: Subject<any> = new Subject();
 
-  constructor(private service: VacanciesService){
-    this.vacancies = [];
+  constructor(private service: VacanciesService,
+              public dialog: MatDialog){
     for (let serviceElement of this.service.vacanciesList) {
       this.vacancies.push(serviceElement);
     }
-    this.showOpened = true;
-    this.showSuspended = true;
-    this.showCanceled = true;
-    this.showClosed = true;
   }
 
-  statusToShow(vacancyStatus: string) {
-    let shownVacancies: VacancyListItem[] = [];
+  openDialog(title: string) {
+    const dialogRef = this.dialog.open(VacancyEditComponent, {
+      width: '650px',
+      data: {dialogTitle: title}
+    });
 
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      console.log(this.vacancies);
+      console.log(result);
+      if (result) {
+        this.vacancies = this.service.vacanciesList;
+        this.statusToShow();
+        this.refresh.next();
+      }
+    });
+  }
+
+  toggle(vacancyStatus: string) {
     switch (vacancyStatus) {
       case 'opened': this.showOpened = !this.showOpened; break;
       case 'suspended': this.showSuspended = !this.showSuspended; break;
       case 'canceled': this.showCanceled = !this.showCanceled; break;
       case 'closed': this.showClosed = !this.showClosed; break;
     }
+  }
+
+  statusToShow() {
+    let shownVacancies: VacancyListItem[] = [];
 
     if (this.showOpened) {
       for (let i = 0; i < this.service.vacanciesList.length; i++) {
--- src/app/vacancy-page/vacancy-edit/vacancy-edit.component.ts
+++ src/app/vacancy-page/vacancy-edit/vacancy-edit.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
 import {VacanciesService} from '../../shared/vacancies.service';
+import {DialogData} from '../../calendar/calendar/calendar.component';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import {VacancyListItem} from '../../vacancies-page/shared/templates';
 
 interface Requirement {
   reqName: string;
@@ -16,11 +19,14 @@ interface Requirement {
 export class VacancyEditComponent implements OnInit {
   vacancyEdit: FormGroup;
   requirements: FormArray;
+  newVacancy: VacancyListItem;
 
   isInactive = false;
 
   constructor(private fb: FormBuilder,
-              private service: VacanciesService) { }
+              private service: VacanciesService,
+              @Inject(MAT_DIALOG_DATA) public data: DialogData,
+              public dialogRef: MatDialogRef<VacancyEditComponent>) { }
 
   ngOnInit() {
     this.vacancyEdit = this.fb.group({
@@ -45,13 +51,15 @@ export class VacancyEditComponent implements OnInit {
   }
 
   onSave() {
-    this.service.addVacancy({
+    this.newVacancy = {
       vacancyName: this.vacancyEdit.get('vacancyName').value,
       vacancyStatus: this.vacancyEdit.get('vacancyStatus').value,
       vacancyDescription: this.vacancyEdit.get('vacancyDescription').value,
       requirements: this.vacancyEdit.get('requirements').value,
       candidates: []
-    });
-  }
+    };
 
+    this.service.addVacancy(this.newVacancy);
+    this.dialogRef.close(this.newVacancy);
+  }
 }
