de6a31ae41893fe84408ba6a350f68f1de35f873
AnastasiyaKul <43112477+AnastasiyaKul@users.noreply.github.com>
2019-01-09 15:52:32 +0300

    мелкие изменения в вёрстке, добавила полный список позиций в фильтр

--- src/app/candidates-page/candidates-filter/candidate-filter.component.ts
+++ src/app/candidates-page/candidates-filter/candidate-filter.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Positions} from '../../vacancies-page/shared/templates';
+import {CandidatesService} from '../../shared/candidates.service';
 
 @Component({
   selector: 'app-candidate-filter',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CandidateFilterComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    for (const pos in Positions) {
+      this.positions.push( pos );
+    }
+  }
+  positions = [];
+  filteredPositions = [];
 
   ngOnInit() {
   }
-
 }
--- src/app/shared/candidates.service.ts
+++ src/app/shared/candidates.service.ts
@@ -43,5 +43,65 @@ export class CandidatesService {
       otherContacts: '+375-25-000-0000',
       notes: 'Good girl'
     },
+    {
+      id: 5,
+      candidateName: 'Bob',
+      candidateSurname: 'Twist',
+      position: Positions.Vue,
+      phone: '+375-25-125-ч1524',
+      mail: 'Twist@gmail.com',
+      otherContacts: 'no',
+      notes: 'Good man'
+    },
+    {
+      id: 6,
+      candidateName: 'Billy',
+      candidateSurname: 'Parker',
+      position: Positions.CSS,
+      phone: '+375-33-321-5560',
+      mail: 'Parkerb@gmail.com',
+      otherContacts: 'Parker Billy - Skype',
+      notes: 'forgot about the job interview'
+    },
+    {
+      id: 7,
+      candidateName: 'Steve',
+      candidateSurname: 'Jobs',
+      position: Positions.Vue,
+      phone: '+375-29-321-0077',
+      mail: 'Jobslol@gmail.com',
+      otherContacts: 'no',
+      notes: ''
+    },
+    {
+      id: 8,
+      candidateName: 'Anna',
+      candidateSurname: 'Pott',
+      position: Positions.Angular,
+      phone: '+375-25-721-5990',
+      mail: 'AnnaPott@gmail.com',
+      otherContacts: 'Anna Pott - fb',
+      notes: 'Everything cool'
+    },
+    {
+      id: 9,
+      candidateName: 'Larry',
+      candidateSurname: 'King',
+      position: Positions.React,
+      phone: '+375-25-125-0124',
+      mail: 'orughoug@gmail.com',
+      otherContacts: 'no',
+      notes: 'Good man'
+    },
+    {
+      id: 10,
+      candidateName: 'Mark',
+      candidateSurname: 'Poster',
+      position: Positions.Angular,
+      phone: '+375-25-321-4587',
+      mail: 'abcdefrg@gmail.com',
+      otherContacts: '',
+      notes: 'Stupid'
+    },
   ];
 }
