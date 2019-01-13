ab8daf0afc97823d505d7a1e11c3f2053f7e0bc4
AnastasiyaKul <43112477+AnastasiyaKul@users.noreply.github.com>
2019-01-13 13:09:01 +0300

    исправила верстку на странице кандидатов

--- src/app/candidates-page/candidates-table/candidates-table.component.ts
+++ src/app/candidates-page/candidates-table/candidates-table.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
 import {CandidatesService} from '../../shared/candidates.service';
 import {Router} from '@angular/router';
 import {CandidateShortInfo} from '../../vacancies-page/shared/templates';
@@ -9,8 +9,11 @@ import {CandidateShortInfo} from '../../vacancies-page/shared/templates';
   styleUrls: ['./candidates-table.component.css'],
 })
 export class CandidatesTableComponent implements OnInit {
+  @Input() isFilteredArr;
+  @Input() filteredCandidatesArr
+
   constructor(private candidatesService: CandidatesService, private router: Router) { }
-  goToCandidate(candidateId:number){
+  goToCandidate(candidateId: number) {
     this.router.navigate(['candidate', candidateId]);
   }
   ngOnInit() {
